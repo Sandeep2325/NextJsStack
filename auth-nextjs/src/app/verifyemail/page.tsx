2173dee23cab943b64c913b9b11683e6b5b8e683
Sandeep2325 <sandeepgowda2314@gmail.com>
2024-02-16 23:50:09 +0530

    Final push2

--- auth-nextjs/src/app/verifyemail/page.tsx
+++ auth-nextjs/src/app/verifyemail/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
-export default function verifyEmailPage() {
+export default function VerifyEmailPage() {
 
     const [token, setToken] = useState("")
     const [verified, setVerified] = useState(false)
